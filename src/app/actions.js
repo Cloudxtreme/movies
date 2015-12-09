7922cf207aee0be24a16a1e7edd0789be90b7e6e
Clay Miller <clay@smockle.com>
2015-12-09 15:12:19 -0800

    Update button color.

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-fetch';
 export const SELECT_MOVIE = 'SELECT_MOVIE';
 export const REQUEST_MOVIE = 'REQUEST_MOVIE';
 export const RECEIVE_MOVIE = 'RECEIVE_MOVIE';
+export const SAVE_MOVIE = 'SAVE_MOVIE';
 
 export function selectMovie(movie) {
   return {
@@ -35,5 +36,12 @@ export function fetchMovie(movie) {
     return fetch(`http://www.omdbapi.com/?t=${encodedMovie}`)
            .then(response => response.json())
            .then(json => dispatch(receiveMovie(movie, json)));
+  }
+}
+export function saveMovie(movie) {
+  return {
+    type: RECEIVE_MOVIE,
+    payload: { movie: movie },
+    error: false
   }
 }
